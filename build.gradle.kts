293f900ac87da0c253938d2445c66a709a1ed8f3
René Jörg Spies <121259924+ReneSpies2@users.noreply.github.com>
2022-12-23 10:34:34 +0100

    DDS-12 Update all stable versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,15 @@
+import nl.littlerobots.vcu.plugin.versionCatalogUpdate
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.littlerobots) apply true
     alias(libs.plugins.benManes.versions) apply true
+}
+
+versionCatalogUpdate {
+    keep {
+        keepUnusedVersions.set(true)
+    }
 }
