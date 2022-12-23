604cb37c67810237936d3211fb9346196289b388
René Jörg Spies <121259924+ReneSpies2@users.noreply.github.com>
2022-12-23 17:10:41 +0100

    Squashed commit of the following:
    commit d9c69a00256d5777085b2c7db8afb63d559dce34
    Author: René Jörg Spies <121259924+ReneSpies2@users.noreply.github.com>
    Date:   Fri Dec 23 16:56:05 2022 +0100
    
        DDS-14 Jack wants to use a Logger (#2)
    
        DDS-14 Added timber

---
+++ app/src/main/java/me/renespies/daedalus/MainApplication.kt
@@ -0,0 +1,11 @@
+package me.renespies.daedalus
+
+import android.app.Application
+import timber.log.Timber
+
+class MainApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
