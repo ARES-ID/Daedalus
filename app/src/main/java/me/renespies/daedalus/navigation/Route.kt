afaf9c89d266e98e05bb980033d9a764b7b9489e
René Jörg Spies <121259924+ReneSpies@users.noreply.github.com>
2023-01-12 22:04:06 +0100

    DAED-21 Revert to flat navigation routes
    Signed-off-by: René Jörg Spies <121259924+ReneSpies@users.noreply.github.com>

--- app/src/main/java/me/renespies/daedalus/navigation/Route.kt
+++ app/src/main/java/me/renespies/daedalus/navigation/Route.kt
@@ -1,13 +1,10 @@
 package me.renespies.daedalus.navigation
 
-sealed class Route(val actual: String) {
-    sealed class GreenEngineeringMenu(subRoute: String) : Route("GreenEngineeringMenu#$subRoute") {
-        object Entry : GreenEngineeringMenu("Entry")
-        object Typography : GreenEngineeringMenu("Typography")
-    }
+typealias Route = String
 
-    sealed class Home(subRoute: String) : Route("Home#$subRoute") {
-        object Entry : Home("Entry")
-        object AddWeight : Home("AddWeight")
-    }
+object Routes {
+    const val GreenEngineeringMenu = "greenEngineeringMenu"
+    const val Typography = "typography"
+    const val Home = "home"
+    const val AddWeight = "addWeight"
 }
