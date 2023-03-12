efa46d308b6ba023b947285b7221b58c1b8633e0
René Jörg Spies <121790537+rjspies@users.noreply.github.com>
2023-03-12 20:39:29 +0100

    DAED-35 Use tableItems
    Signed-off-by: René Jörg Spies <121790537+rjspies@users.noreply.github.com>

--- app/src/main/java/me/renespies/daedalus/weight/weighthistory/WeightHistoryScreen.kt
+++ app/src/main/java/me/renespies/daedalus/weight/weighthistory/WeightHistoryScreen.kt
@@ -1,22 +1,15 @@
 package me.renespies.daedalus.weight.weighthistory
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
-import kotlinx.coroutines.launch
 import me.renespies.daedalus.compose.ToolbarContent
-import me.renespies.daedalus.ui.theme.Spacings
+import me.renespies.daedalus.compose.tableItems
 import java.time.ZonedDateTime
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -25,25 +18,19 @@ fun WeightHistoryScreen(
     onBack: () -> Unit,
     viewModel: WeightHistoryViewModel = viewModel(factory = WeightHistoryViewModel.Factory)
 ) {
-    ToolbarContent(title = "Weight history", onBack = onBack) {
-        Column(Modifier.verticalScroll(rememberScrollState())) {
-            val scope = rememberCoroutineScope()
-            Button(onClick = {
-                scope.launch {
-                    viewModel.clearWeights()
-                }
-            }) {
-                Text(text = "clear")
-            }
-            val weights by viewModel.weights.collectAsState()
-            weights.forEach {
-                Card(Modifier.padding(Spacings.M)) {
+    ToolbarContent(title = "Verlauf", onBack = onBack) {
+        val weights by viewModel.weights.collectAsState()
+        LazyColumn {
+            tableItems(
+                items = weights,
+                key = { it.id },
+                itemContent = {
                     Column {
                         Text(text = it.weight.toString())
                         Text(text = it.dateTime.userfacingString)
                     }
                 }
-            }
+            )
         }
     }
 }
