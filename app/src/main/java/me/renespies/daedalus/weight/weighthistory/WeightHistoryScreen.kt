4a3bde7937bd7666495bc5f1c7041fae20abf677
René Jörg Spies <121790537+rjspies@users.noreply.github.com>
2023-03-12 21:19:00 +0100

    DAED-35 Styling
    Signed-off-by: René Jörg Spies <121790537+rjspies@users.noreply.github.com>

--- app/src/main/java/me/renespies/daedalus/weight/weighthistory/WeightHistoryScreen.kt
+++ app/src/main/java/me/renespies/daedalus/weight/weighthistory/WeightHistoryScreen.kt
@@ -1,16 +1,33 @@
 package me.renespies.daedalus.weight.weighthistory
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ArrowUpward
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import androidx.lifecycle.viewmodel.compose.viewModel
 import me.renespies.daedalus.compose.ToolbarContent
 import me.renespies.daedalus.compose.tableItems
+import me.renespies.daedalus.ui.theme.Spacings
+import me.renespies.daedalus.weight.service.data.Weight
 import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -24,16 +41,65 @@ fun WeightHistoryScreen(
             tableItems(
                 items = weights,
                 key = { it.id },
-                itemContent = {
-                    Column {
-                        Text(text = it.weight.toString())
-                        Text(text = it.dateTime.userfacingString)
-                    }
-                }
+                itemContent = { WeightRow(weight = it) }
             )
         }
     }
 }
 
+@Composable
+private fun WeightRow(weight: Weight) {
+    ConstraintLayout(Modifier.fillMaxWidth()) {
+        val (avatar, title, date) = createRefs()
+        Avatar(
+            modifier = Modifier.constrainAs(avatar) {
+                top.linkTo(parent.top, margin = Spacings.M)
+                start.linkTo(parent.start, margin = Spacings.M)
+                bottom.linkTo(parent.bottom, margin = Spacings.M)
+            }
+        )
+        Text(
+            text = weight.weight.userfacingString,
+            style = MaterialTheme.typography.titleLarge,
+            modifier = Modifier.constrainAs(title) {
+                width = Dimension.fillToConstraints
+                top.linkTo(parent.top, margin = Spacings.M)
+                start.linkTo(avatar.end, margin = Spacings.M)
+                end.linkTo(parent.end, margin = Spacings.M)
+            }
+        )
+        Text(
+            text = weight.dateTime.userfacingString,
+            style = MaterialTheme.typography.titleSmall,
+            modifier = Modifier.constrainAs(date) {
+                width = Dimension.fillToConstraints
+                top.linkTo(title.bottom)
+                start.linkTo(avatar.end, margin = Spacings.M)
+                bottom.linkTo(parent.bottom, margin = Spacings.M)
+                end.linkTo(parent.end, margin = Spacings.M)
+            }
+        )
+    }
+}
+
+@Composable
+private fun Avatar(modifier: Modifier = Modifier) {
+    Box(
+        modifier = Modifier
+            .clip(CircleShape)
+            .background(Color.Gray.copy(alpha = .3f))
+            .then(modifier)
+    ) {
+        Icon(
+            imageVector = Icons.Outlined.ArrowUpward,
+            contentDescription = null,
+            modifier = Modifier.padding(Spacings.S)
+        )
+    }
+}
+
+val Float.userfacingString: String
+    get() = "${toString()} kg"
+
 val ZonedDateTime.userfacingString: String
-    get() = "$year-$monthValue-$dayOfYear $hour:$minute:$second"
+    get() = format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM))
