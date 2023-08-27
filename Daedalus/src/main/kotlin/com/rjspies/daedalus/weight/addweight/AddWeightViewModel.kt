898b74ea76ab04e77cac6e805595acb9ffbc8aa6
René Jörg Spies <121790537+rjspies@users.noreply.github.com>
2023-08-27 23:16:18 +0200

    DAED-76 Inject AddWeightViewModel

--- Daedalus/src/main/kotlin/com/rjspies/daedalus/weight/addweight/AddWeightViewModel.kt
+++ Daedalus/src/main/kotlin/com/rjspies/daedalus/weight/addweight/AddWeightViewModel.kt
@@ -1,22 +1,11 @@
 package com.rjspies.daedalus.weight.addweight
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
-import androidx.lifecycle.viewmodel.initializer
-import androidx.lifecycle.viewmodel.viewModelFactory
-import com.rjspies.daedalus.MainApplication
 import com.rjspies.daedalus.weight.service.WeightService
 import com.rjspies.daedalus.weight.service.data.Weight
+import org.koin.android.annotation.KoinViewModel
 
-class AddWeightViewModel private constructor(private val service: WeightService) : ViewModel() {
+@KoinViewModel
+class AddWeightViewModel(private val service: WeightService) : ViewModel() {
     suspend fun saveWeight(weight: Weight) = service.saveWeight(weight)
-
-    companion object {
-        val Factory = viewModelFactory {
-            initializer {
-                val application = get(APPLICATION_KEY) as MainApplication
-                AddWeightViewModel(application.weightService)
-            }
-        }
-    }
 }
