001306bee9690d4da56476b84bce8c09663f464e
René Jörg Spies <121790537+rjspies@users.noreply.github.com>
2023-08-27 23:14:24 +0200

    DAED-76 Inject WeightGraphViewModel

--- Daedalus/src/main/kotlin/com/rjspies/daedalus/weight/weightgraph/WeightGraphScreen.kt
+++ Daedalus/src/main/kotlin/com/rjspies/daedalus/weight/weightgraph/WeightGraphScreen.kt
@@ -8,7 +8,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.platform.LocalContext
 import androidx.core.content.res.ResourcesCompat
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.patrykandpatrick.vico.compose.axis.axisLabelComponent
 import com.patrykandpatrick.vico.compose.axis.horizontal.rememberBottomAxis
 import com.patrykandpatrick.vico.compose.axis.vertical.rememberStartAxis
@@ -18,11 +17,13 @@ import com.patrykandpatrick.vico.core.chart.values.AxisValuesOverrider
 import com.patrykandpatrick.vico.core.entry.ChartEntryModelProducer
 import com.rjspies.daedalus.R
 import com.rjspies.daedalus.weight.weightgraph.ui.rememberMarker
+import org.koin.androidx.compose.koinViewModel
 
-private const val YAxisValuePadding = 1.01f
+private const val Y_AXIS_VALUE_PADDING = 1.01f
 
 @Composable
-fun WeightGraphScreen(viewModel: WeightGraphViewModel = viewModel(factory = WeightGraphViewModel.Factory)) {
+fun WeightGraphScreen() {
+    val viewModel: WeightGraphViewModel = koinViewModel()
     val weights by viewModel.weights.collectAsState()
     val entries = rememberSaveable(weights) {
         weights.mapIndexed { index, weight ->
@@ -36,7 +37,7 @@ fun WeightGraphScreen(viewModel: WeightGraphViewModel = viewModel(factory = Weig
     val context = LocalContext.current
     val lineProducer = remember(weights) { ChartEntryModelProducer(entries) }
     val axisFormatter = remember { WeightDateAxisFormatter }
-    val valuesOverrider = remember { AxisValuesOverrider.adaptiveYValues(YAxisValuePadding) }
+    val valuesOverrider = remember { AxisValuesOverrider.adaptiveYValues(Y_AXIS_VALUE_PADDING) }
     val typeface = remember { ResourcesCompat.getFont(context, R.font.poppins_regular) ?: Typeface.MONOSPACE }
     val axisLabel = axisLabelComponent(typeface = typeface)
 
