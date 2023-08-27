7dfd0e107e4d9c1d146a04a74d582321e5382c8f
René Jörg Spies <121790537+rjspies@users.noreply.github.com>
2023-08-27 16:38:34 +0200

    DAED-76 Setup koin

--- Daedalus/src/main/kotlin/com/rjspies/daedalus/MainApplication.kt
+++ Daedalus/src/main/kotlin/com/rjspies/daedalus/MainApplication.kt
@@ -2,8 +2,13 @@ package com.rjspies.daedalus
 
 import android.app.Application
 import com.rjspies.daedalus.database.WeightDatabase
+import com.rjspies.daedalus.koin.AppModule
 import com.rjspies.daedalus.weight.service.WeightService
 import com.rjspies.daedalus.weight.service.data.WeightDao
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.ksp.generated.module
 import timber.log.Timber
 
 class MainApplication : Application() {
@@ -20,5 +25,12 @@ class MainApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
+        startKoin()
+    }
+
+    private fun startKoin() = startKoin {
+        androidLogger()
+        androidContext(applicationContext)
+        modules(AppModule().module)
     }
 }
---
+++ Daedalus/src/main/kotlin/com/rjspies/daedalus/koin/AppModule.kt
@@ -0,0 +1,8 @@
+package com.rjspies.daedalus.koin
+
+import org.koin.core.annotation.ComponentScan
+import org.koin.core.annotation.Module
+
+@Module
+@ComponentScan("com.rjspies.daedalus")
+class AppModule
