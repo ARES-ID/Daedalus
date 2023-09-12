95c8c9cc361f51127c752d527e0920737d235744
René Jörg Spies <121790537+rjspies@users.noreply.github.com>
2023-09-12 23:58:00 +0200

    DAED-87 Use master instead of main

--- Daedalus/build.gradle.kts
+++ Daedalus/build.gradle.kts
@@ -99,7 +99,7 @@ ksp {
 fun generateVersionCode(): Int {
     val standardOutput = ByteArrayOutputStream()
     rootProject.exec {
-        commandLine("git", "rev-list", "--count", "main")
+        commandLine("git", "rev-list", "--count", "master")
         this.standardOutput = standardOutput
     }
     val commitCount = standardOutput.toString().trim().toInt()
