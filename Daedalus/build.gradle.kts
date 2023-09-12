749de07f5fdbb64c21fa8aa26ad9093f155e356b
René Jörg Spies <121790537+rjspies@users.noreply.github.com>
2023-09-13 00:00:10 +0200

    DAED-87 Use HEAD again

--- Daedalus/build.gradle.kts
+++ Daedalus/build.gradle.kts
@@ -99,7 +99,7 @@ ksp {
 fun generateVersionCode(): Int {
     val standardOutput = ByteArrayOutputStream()
     rootProject.exec {
-        commandLine("git", "rev-list", "--count", "master")
+        commandLine("git", "rev-list", "--count", "HEAD")
         this.standardOutput = standardOutput
     }
     return standardOutput.toString().trim().toInt()
