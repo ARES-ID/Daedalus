ad3145e0ec623c4fa4e5783e5145953a95d7daa3
René Jörg Spies <121790537+rjspies@users.noreply.github.com>
2023-11-07 21:43:27 +0100

    Test parameters

--- .jenkins/build.groovy
+++ .jenkins/build.groovy
@@ -1,7 +1,16 @@
 pipeline {
     agent any
-
+    parameters {
+        string(name: 'REPO_URL', defaultValue: 'https://github.com/rjspies/Daedalus.git', description: '')
+        string(name: 'BRANCH', defaultValue: 'Jenkins-test', description: '')
+        string(name: 'JENKINSFILE_PATH', defaultValue: '.jenkins/build.groovy', description: '')
+    }
     stages {
+        stage("Checkout"){
+            steps {
+                git branch: "${params.BRANCH}", url: "${params.REPO_URL}"
+            }
+        }
         stage("Kotlinter") {
             steps {
                 sh "./gradlew lintKotlin"
