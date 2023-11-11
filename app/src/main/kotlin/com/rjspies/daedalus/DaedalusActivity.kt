5336c200fe234fd68de26b527ca0238507109df7
René Jörg Spies <121790537+rjspies@users.noreply.github.com>
2023-11-11 23:48:00 +0100

    DAED-97 Add splash screen exit animation

--- app/src/main/kotlin/com/rjspies/daedalus/DaedalusActivity.kt
+++ app/src/main/kotlin/com/rjspies/daedalus/DaedalusActivity.kt
@@ -1,17 +1,23 @@
 package com.rjspies.daedalus
 
+import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.animation.LinearInterpolator
+import android.window.SplashScreen
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.core.animation.doOnEnd
 import androidx.core.view.WindowCompat
-import androidx.navigation.compose.rememberNavController
 import com.rjspies.daedalus.navigation.NavigationHost
 import com.rjspies.daedalus.ui.theme.DaedalusTheme
 import com.rjspies.daedalus.ui.widgets.DaedalusSnackbar
@@ -22,9 +28,13 @@ class DaedalusActivity : ComponentActivity() {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         setTheme(R.style.Theme_Daedalus)
         super.onCreate(savedInstanceState)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            splashScreen.animateExit()
+        }
+
         setContent {
             DaedalusTheme {
-                val navigationController = rememberNavController()
                 val snackbarHostState = remember { SnackbarHostState() }
                 Scaffold(
                     modifier = Modifier.fillMaxSize(),
@@ -35,9 +45,25 @@ class DaedalusActivity : ComponentActivity() {
                             snackbar = { DaedalusSnackbar(it) },
                         )
                     },
-                    content = { NavigationHost(navigationController, snackbarHostState) },
+                    content = { NavigationHost(snackbarHostState) },
                 )
             }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    private fun SplashScreen.animateExit() {
+        setOnExitAnimationListener { view ->
+            val slideDownAnimation = ObjectAnimator.ofFloat(
+                /* target = */ view,
+                /* property = */ View.TRANSLATION_Y,
+                /* ...values = */ 0f,
+                view.height.toFloat(),
+            )
+            slideDownAnimation.interpolator = LinearInterpolator()
+            slideDownAnimation.duration = 200L
+            slideDownAnimation.doOnEnd { view.remove() }
+            slideDownAnimation.start()
         }
     }
 }
