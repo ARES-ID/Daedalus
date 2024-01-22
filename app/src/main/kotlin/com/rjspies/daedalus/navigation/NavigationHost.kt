b4817489e8034dbac71b4fff4821b955d1acb002
René Jörg Spies <121790537+rjspies@users.noreply.github.com>
2024-01-22 22:54:52 +0100

    DAED-117 Fix lint
    Signed-off-by: René Jörg Spies <121790537+rjspies@users.noreply.github.com>

--- app/src/main/kotlin/com/rjspies/daedalus/navigation/NavigationHost.kt
+++ app/src/main/kotlin/com/rjspies/daedalus/navigation/NavigationHost.kt
@@ -4,10 +4,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
-import com.rjspies.daedalus.R
 import com.rjspies.daedalus.compose.greenEngineeringMenuGestureDetector
 import com.rjspies.daedalus.greenengineeringmenu.ButtonGalleryScreen
 import com.rjspies.daedalus.greenengineeringmenu.GreenEngineeringMenuScreen
