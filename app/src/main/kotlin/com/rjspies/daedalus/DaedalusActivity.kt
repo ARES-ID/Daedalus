7c23cacec60b46ee1d2c2ed7594a71094caafea8
René Jörg Spies <121790537+rjspies@users.noreply.github.com>
2024-02-04 12:07:27 +0100

    DAED-145 Add BottomAppBar

--- app/src/main/kotlin/com/rjspies/daedalus/DaedalusActivity.kt
+++ app/src/main/kotlin/com/rjspies/daedalus/DaedalusActivity.kt
@@ -11,9 +11,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.rounded.List
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material.icons.rounded.AreaChart
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
@@ -61,7 +71,34 @@ class DaedalusActivity : ComponentActivity() {
                                 },
                             )
                         },
-                        content = { NavigationHost(snackbarHostState) },
+                        bottomBar = {
+                            BottomAppBar(
+                                floatingActionButton = {
+                                    FloatingActionButton(onClick = { /*TODO*/ }) {
+                                        Icon(imageVector = Icons.Rounded.Add, contentDescription = null)
+                                    }
+                                },
+                                actions = {
+                                    IconButton(onClick = { /*TODO*/ }) {
+                                        Icon(
+                                            imageVector = Icons.Rounded.AreaChart,
+                                            contentDescription = null,
+                                        )
+                                    }
+                                    IconButton(onClick = { /*TODO*/ }) {
+                                        Icon(
+                                            imageVector = Icons.AutoMirrored.Rounded.List,
+                                            contentDescription = null,
+                                        )
+                                    }
+                                },
+                            )
+                        },
+                        content = {
+                            Box(Modifier.padding(it)) {
+                                NavigationHost(snackbarHostState)
+                            }
+                        },
                     )
                 }
             }
